bb1043d939eed01c73039b1745db8433a5fd4da1
郑伯昕 <b0@zhengboxindeMacBook-Air.local>
2026-09-02 21:27:18 +0800

    Initial Accel-SCOPE standalone snapshot
    Source: bbooxxx/accel-sim-framework@38ae5d30da8948318827598bb4e0860435038c9f

---
+++ gpu-simulator/setup_environment.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+# Copyright (c) 2020 Timothy Rogers, Purdue University
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+# Redistributions in binary form must reproduce the above copyright notice, this
+# list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
+# Neither the name of The University of British Columbia nor the names of its
+# contributors may be used to endorse or promote products derived from this
+# software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+export ACCELSIM_SETUP_ENVIRONMENT_WAS_RUN=
+export ACCELSIM_ROOT="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
+
+#   Different branches of Accel-Sim should have different values here
+#   For development, we use our internal repo and the dev branch
+#       Ideally, when we release, it should be based off a GPGPU-Sim release.
+export GPGPUSIM_REPO="${GPGPUSIM_REPO:=https://github.com/sun-lingyu/gpgpu-sim_distribution.git}"
+export GPGPUSIM_BRANCH="${GPGPUSIM_BRANCH:=dev}"
+export GPGPUSIM_COMMIT="${GPGPUSIM_COMMIT:=09c0fea40c0afb3c44c3eb44198270901e3ead72}"
+
+gpgpu_git() {
+    (cd "$ACCELSIM_ROOT/gpgpu-sim" && git "$@")
+}
+
+if [ $# = '1' ] ;
+then
+    export ACCELSIM_CONFIG=$1
+else
+    export ACCELSIM_CONFIG=release
+fi
+
+# If we can't find an already set version of GPGPU-Sim, then pull one locally using the repos specificed above
+if [ -z "$GPGPUSIM_SETUP_ENVIRONMENT_WAS_RUN" -o ! -d "$GPGPUSIM_ROOT" ]; then
+    echo "No \$GPGPUSIM_ROOT, testing for local folder in: \"$ACCELSIM_ROOT/gpgpu-sim\""
+    if [ ! -d "$ACCELSIM_ROOT/gpgpu-sim" ] ; then
+        echo "No \$ACCELSIM_ROOT/gpgpu-sim."
+        if [ ! -z "$PS1" ]; then
+            read -e -p "Please specify the repo you want to sync for GPGPU-Sim (default is $GPGPUSIM_REPO):" user_repo
+        fi
+        if [ -z $user_repo ] ; then
+            user_repo=$GPGPUSIM_REPO
+        fi
+
+        if [ ! -z "$PS1" ]; then
+            read -e -p "Please specify the branch for GPGPU-Sim you would like to use (default is $GPGPUSIM_BRANCH):" user_branch
+        fi
+        if [ -z $user_branch ] ; then
+            user_branch=$GPGPUSIM_BRANCH
+        fi
+        git clone "$user_repo" "$ACCELSIM_ROOT/gpgpu-sim"
+        gpgpu_git checkout "$user_branch"
+        gpgpu_git checkout "$GPGPUSIM_COMMIT"
+    else
+        echo "Found $ACCELSIM_ROOT/gpgpu-sim, using existing local location. Not sycning anything."
+    fi
+    accel_scope_patch="$ACCELSIM_ROOT/patches/accel-scope-gpgpu-sim.patch"
+    if [ -f "$accel_scope_patch" ]; then
+        if gpgpu_git apply --check "$accel_scope_patch" 2>/dev/null; then
+            gpgpu_git apply "$accel_scope_patch" || return 1
+            echo "Applied the Accel-SCOPE GPGPU-Sim patch."
+        elif gpgpu_git apply --reverse --check "$accel_scope_patch" 2>/dev/null; then
+            echo "Accel-SCOPE GPGPU-Sim patch is already applied."
+        else
+            echo "ERROR: the Accel-SCOPE patch does not match the local GPGPU-Sim tree." >&2
+            return 1
+        fi
+    fi
+    source "$ACCELSIM_ROOT/gpgpu-sim/setup_environment" "$ACCELSIM_CONFIG" || return 1
+else
+    source "$GPGPUSIM_ROOT/setup_environment" "$ACCELSIM_CONFIG" || return 1
+fi
+
+if [ ! -d "$ACCELSIM_ROOT/extern/pybind11" ] ; then
+    git clone --depth 1 -b master https://github.com/pybind/pybind11.git $ACCELSIM_ROOT/extern/pybind11
+fi
+
+export PYTHONPATH=$ACCELSIM_ROOT/build/$ACCELSIM_CONFIG:$PYTHONPATH
+
+echo "Using GPGPU-Sim in $GPGPUSIM_ROOT"
+#echo "If that is not the intended behavior, then run: \"unset GPGPUSIM_ROOT; unset GPGPUSIM_SETUP_ENVIRONMENT_WAS_RUN\"."
+
+echo "Accel-Sim setup succeeded."
+export ACCELSIM_SETUP_ENVIRONMENT_WAS_RUN=1
+
+# added for slurm
+export TORQUE_QUEUE_NAME=sail3090
---
+++ util/tuner/GPU_Microbenchmark/hw_def/hw_def.h
@@ -0,0 +1,20 @@
+#ifndef HW_DEF_H
+#define HW_DEF_H
+
+//#include "kepler_TITAN_hw_def.h"
+
+//#include "pascal_TITANX_hw_def.h"
+
+//#include "volta_QV100_hw_def.h"
+
+//#include "turing_RTX2060_hw_def.h"
+
+//#include "ampere_RTX3070_hw_def.h"
+
+//#include "volta_TITANV_hw_def.h"
+
+// #include "volta_XAVIER_hw_def.h"
+
+#include "ampere_ORIN_hw_def.h"
+
+#endif
